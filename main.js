d5743c3ffd460bd491dc0c855cb0df42d0ec3ada
NatiaMinasiani <natiaminasiani@gmail.com>
2025-02-14 19:15:32 +0300

    Added js code

---
+++ main.js
@@ -0,0 +1,31 @@
+let userEmail = "example@gmail.com";
+let userCountry = "Georgia";
+
+console.log(userEmail);
+const userLastName = "Minasiani";
+const userAge = 23;
+const isUserEmployed = true;
+const userSalary = undefined;
+const userAddress = null;
+
+//დამატებითი ცვლადები;
+
+const userName = "Natia";
+const dayOfWeek = "friday";
+const FavoriteActivity = " workout";
+
+const message = " Hi " + "my " + "name " + "is " + userName;
+console.log(message);
+
+const message2 = "Today " + "is " + dayOfWeek;
+console.log(message2);
+
+const message3 = "A " + "perfect " + "day " + "for" + FavoriteActivity;
+console.log(message3);
+
+const message4 = `Today is ${dayOfWeek}`;
+console.log(message4);
+const message5 = `Hi my name is ${userName}`;
+console.log(message5);
+const message6 = `A perfect day for${FavoriteActivity}`;
+console.log(message6);
